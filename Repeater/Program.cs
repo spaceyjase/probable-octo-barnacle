232a38b2c970bcfe1b617154917c561dc141d6ca
Jason A <429978+spaceyjase@users.noreply.github.com>
2020-03-31 16:07:07 +0100

    Initial checkin of Repeater test

---
+++ Repeater/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Repeater
+{
+  public static class TaskRepeater
+  {
+    public static void StartNew(Action action,
+          TimeSpan pollInterval,
+          CancellationToken token,
+          TaskCreationOptions taskCreationOptions = TaskCreationOptions.None)
+    {
+      Task.Factory.StartNew(
+          () =>
+          {
+            while (true)
+            {
+              try
+              {
+                action();
+
+                if (token.IsCancellationRequested) break;
+                if (token.WaitHandle.WaitOne(pollInterval)) break;
+              }
+              catch
+              {
+                return;
+              }
+            }
+          },
+          token,
+          taskCreationOptions,
+          TaskScheduler.Default);
+    }
+  }
+
+  public class Program
+  {
+    public static void Main()
+    {
+      var cts = new CancellationTokenSource();
+      TaskRepeater.StartNew(() => Console.WriteLine("ping"), TimeSpan.FromMilliseconds(1000), cts.Token);
+
+      Thread.Sleep(5000);
+
+      cts.Cancel();
+
+      Console.WriteLine("done");
+    }
+  }
+}
